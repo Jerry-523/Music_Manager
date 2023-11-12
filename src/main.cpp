edb314e1f3e025b8cbd317924d887747651ed2d9
Péricles Borges <92488227+Jerry-523@users.noreply.github.com>
2023-11-12 15:51:04 -0100

    ...

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int opcao;
+    cout << "   =======[MENU]======\n\n";
+    cout << " [1] => Registrar Dados\n";
+    cout << " [2] => Listar Dados\n";
+    cout << " [0] => Sair do Programa\n";
+    cout << " [Selecionado]: ";
+    cin >> opcao;
+    
+}
